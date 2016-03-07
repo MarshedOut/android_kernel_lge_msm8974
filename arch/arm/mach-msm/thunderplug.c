0979698e0d840716318f9983ffd5f6300ce72a50
Yuri Sh <yuri@bynet.co.il>
2016-03-07 15:19:55 -0500

    Thunderplug: Add touch boost interface and rework parameters
    Changes as per dori's commit:
    * Now gov will respond by enabling cores when touch is registered
    * Added sysfs for touch boost
    * Pushed all values to parm struct
    
    Differences from dorimanx's variant (overall):
    -Change debug from a define to a module parameter so users can
    enable/disable on the fly
    -Change CONFIG_NR_CPUS to NR_CPUS
    -Inlined variables where relevant
    -Keep nr_cpu_online = num_online_cpus() out of for loop to avoid
    pointless recalculation
    -Suspend/resume max CPUs dropped, automatically puts CPUs other than 0/1
    to sleep
    -Init/exit structure

--- arch/arm/mach-msm/thunderplug.c
+++ arch/arm/mach-msm/thunderplug.c
@@ -38,23 +38,19 @@ static struct notifier_block thunder_state_notif;
 #define MIN_CPU_UP_TIME				(300)
 #define MIN_SAMPLING_MS				(10)
 
+#define DEFAULT_BOOST_LOCK_DUR		500 * 1000L
+#define DEFAULT_NR_CPUS_BOOSTED		2
+#define MIN_INPUT_INTERVAL		150 * 1000L
+
 static bool isSuspended = false;
 
-static int max_core_online = NR_CPUS;
-static int min_core_online = 1;
 static int now[8], last_time[8];
-static int sampling_time = DEFAULT_SAMPLING_MS;
-static int load_threshold = DEFAULT_CPU_LOAD_THRESHOLD;
-static int tplug_hp_enabled = HOTPLUG_ENABLED;
-static int hotplug_suspend = 0;
-
 struct cpufreq_policy old_policy[NR_CPUS];
-
 static struct workqueue_struct *tplug_wq;
 static struct delayed_work tplug_work;
 
 static unsigned int last_load[8] = { 0 };
-static unsigned int down_lock_dur = DOWN_LOCK_DUR;
+static u64 last_boost_time;
 
 static unsigned int debug;
 module_param_named(debug_mask, debug, uint, 0644);
@@ -64,6 +60,31 @@ do { 				\
 	if (debug)		\
 		pr_info(msg);	\
 } while (0)
+
+static struct thunder_param_struct {
+	unsigned int cpus_boosted;
+	unsigned int target_cpus;
+	u64 boost_lock_dur;
+	u64 last_input;
+	int hotplug_suspend;
+	unsigned int down_lock_dur;
+	unsigned int sampling_time;
+	int max_core_online;
+	int min_core_online;
+	int tplug_hp_enabled;
+	int load_threshold;
+	struct work_struct up_work;
+} thunder_param = {
+	.cpus_boosted = DEFAULT_NR_CPUS_BOOSTED,
+	.boost_lock_dur = DEFAULT_BOOST_LOCK_DUR,
+	.hotplug_suspend = 0,
+	.down_lock_dur = DOWN_LOCK_DUR,
+	.max_core_online = NR_CPUS,
+	.min_core_online = 1,
+	.sampling_time = DEF_SAMPLING_MS,
+	.tplug_hp_enabled = HOTPLUG_ENABLED,
+	.load_threshold = DEFAULT_CPU_LOAD_THRESHOLD,
+};
 
 struct cpu_load_data {
 	u64 prev_cpu_idle;
@@ -88,7 +109,7 @@ static void apply_down_lock(unsigned int cpu)
 
 	dl->locked = 1;
 	queue_delayed_work_on(0, tplug_wq, &dl->lock_rem,
-			      msecs_to_jiffies(down_lock_dur));
+			      msecs_to_jiffies(thunder_param.down_lock_dur));
 }
 
 static void remove_down_lock(struct work_struct *work)
@@ -112,7 +133,7 @@ static inline void cpus_bring_offline(unsigned int sus)
 				continue;
 		cpu_down(cpu);
 	}
-	dprintk("%s: cpus were offlined\n", THUNDERPLUG);
+	dprintk("%s: cpus set offline.\n", THUNDERPLUG);
 }
 
 static inline void cpus_bring_online(unsigned int sus)
@@ -130,13 +151,13 @@ static inline void cpus_bring_online(unsigned int sus)
 		cpu_up(cpu);
 		apply_down_lock(cpu);
 	}
-	dprintk("%s: cpus were onlined\n", THUNDERPLUG);
+	dprintk("%s: cpus set online.\n", THUNDERPLUG);
 }
 
 static ssize_t thunderplug_hotplug_suspend_show(struct kobject *kobj,
                         struct kobj_attribute *attr, char *buf)
 {
-	return sprintf(buf, "%d", hotplug_suspend);
+	return sprintf(buf, "%d", thunder_param.hotplug_suspend);
 }
 
 static ssize_t thunderplug_hotplug_suspend_store(struct kobject *kobj,
@@ -150,12 +171,12 @@ static ssize_t thunderplug_hotplug_suspend_store(struct kobject *kobj,
 	switch(val) {
 		case 0:
 		case 1:
-			hotplug_suspend = val;
+			thunder_param.hotplug_suspend = val;
 			break;
 		default:
-			dprintk("%s: invalid hotplug_suspend value use 1 or 0\n",
+			dprintk("%s: invalid value, use 1 or 0.\n",
 					THUNDERPLUG);
-			hotplug_suspend = 0;
+			thunder_param.hotplug_suspend = 0;
 			break;
 	}
 	return count;
@@ -164,7 +185,7 @@ static ssize_t thunderplug_hotplug_suspend_store(struct kobject *kobj,
 static ssize_t thunderplug_max_core_online_show(struct kobject *kobj,
 			struct kobj_attribute *attr, char *buf)
 {
-	return sprintf(buf, "%d", max_core_online);
+	return sprintf(buf, "%d", thunder_param.max_core_online);
 }
 
 static ssize_t __ref thunderplug_max_core_online_store(struct kobject *kobj,
@@ -180,8 +201,9 @@ static ssize_t __ref thunderplug_max_core_online_store(struct kobject *kobj,
 		case 2:
 		case 3:
 		case 4:
-			if (tplug_hp_enabled && max_core_online != val) {
-				max_core_online = val;
+			if (thunder_param.tplug_hp_enabled &&
+					thunder_param.max_core_online != val) {
+				thunder_param.max_core_online = val;
 				cpus_bring_offline(0);
 				cpus_bring_online(0);
 			}
@@ -197,7 +219,7 @@ static ssize_t __ref thunderplug_max_core_online_store(struct kobject *kobj,
 static ssize_t thunderplug_min_core_online_show(struct kobject *kobj,
 			struct kobj_attribute *attr, char *buf)
 {
-	return sprintf(buf, "%d", min_core_online);
+	return sprintf(buf, "%d", thunder_param.min_core_online);
 }
 
 static ssize_t __ref thunderplug_min_core_online_store(struct kobject *kobj,
@@ -213,8 +235,9 @@ static ssize_t __ref thunderplug_min_core_online_store(struct kobject *kobj,
 		case 2:
 		case 3:
 		case 4:
-			if (tplug_hp_enabled && min_core_online != val) {
-				min_core_online = val;
+			if (thunder_param.tplug_hp_enabled &&
+					thunder_param.min_core_online != val) {
+				thunder_param.min_core_online = val;
 				cpus_bring_offline(0);
 				cpus_bring_online(0);
 			}
@@ -230,7 +253,7 @@ static ssize_t __ref thunderplug_min_core_online_store(struct kobject *kobj,
 static ssize_t thunderplug_sampling_show(struct kobject *kobj,
 			struct kobj_attribute *attr, char *buf)
 {
-	return sprintf(buf, "%d", sampling_time);
+	return sprintf(buf, "%d", thunder_param.sampling_time);
 }
 
 static ssize_t thunderplug_sampling_store(struct kobject *kobj,
@@ -242,7 +265,7 @@ static ssize_t thunderplug_sampling_store(struct kobject *kobj,
 	sscanf(buf, "%d", &val);
 
 	if (val >= MIN_SAMPLING_MS)
-		sampling_time = val;
+		thunder_param.sampling_time = val;
 
 	return count;
 }
@@ -250,7 +273,7 @@ static ssize_t thunderplug_sampling_store(struct kobject *kobj,
 static ssize_t thunderplug_load_show(struct kobject *kobj,
 			struct kobj_attribute *attr, char *buf)
 {
-	return sprintf(buf, "%d", load_threshold);
+	return sprintf(buf, "%d", thunder_param.load_threshold);
 }
 
 static ssize_t thunderplug_load_store(struct kobject *kobj,
@@ -262,7 +285,52 @@ static ssize_t thunderplug_load_store(struct kobject *kobj,
 	sscanf(buf, "%d", &val);
 
 	if (val > 10)
-		load_threshold = val;
+		thunder_param.load_threshold = val;
+
+	return count;
+}
+
+static ssize_t thunderplug_boost_lock_duration_show(struct kobject *kobj,
+			struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%llu\n",
+			div_u64(thunder_param.boost_lock_dur, 1000));
+}
+
+static ssize_t thunderplug_boost_lock_duration_store(struct kobject *kobj,
+			struct kobj_attribute *attr,
+			const char *buf, size_t count)
+{
+	int ret;
+	u64 val;
+
+	ret = sscanf(buf, "%llu", &val);
+	if (ret != 1)
+		return -EINVAL;
+
+	thunder_param.boost_lock_dur = val * 1000;
+
+	return count;
+}
+
+static ssize_t thunderplug_cpus_boosted_show(struct kobject *kobj,
+			struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%u\n", thunder_param.cpus_boosted);
+}
+
+static ssize_t thunderplug_cpus_boosted_store(struct kobject *kobj,
+			struct kobj_attribute *attr,
+			const char *buf, size_t count)
+{
+	int ret;
+	unsigned int val;
+
+	ret = sscanf(buf, "%u", &val);
+	if (ret != 1 || val < 1 || val > NR_CPUS)
+		return -EINVAL;
+
+	thunder_param.cpus_boosted = val;
 
 	return count;
 }
@@ -300,12 +368,13 @@ static void __cpuinit tplug_work_fn(struct work_struct *work)
 {
 	int i;
 	unsigned int load[8], avg_load[8];
-	int nr_cpu_online;
+	unsigned int nr_cpu_online;
+	u64 time_now;
 
-	if (!tplug_hp_enabled)
+	if (!thunder_param.tplug_hp_enabled)
 		return;
 
-	for (i = 0 ; i < max_core_online - 1; i++) {
+	for (i = 0 ; i < thunder_param.max_core_online - 1; i++) {
 		if (cpu_online(i))
 			load[i] = get_curr_load(i);
 		else
@@ -318,18 +387,30 @@ static void __cpuinit tplug_work_fn(struct work_struct *work)
 	/* count online cores */
 	nr_cpu_online = num_online_cpus();
 
-	for (i = 0 ; i < max_core_online - 1; i++) {
+	for (i = 0 ; i < thunder_param.max_core_online - 1; i++) {
 		if (cpu_online(i) && avg_load[i] >
-				load_threshold && cpu_is_offline(i + 1)) {
+				thunder_param.load_threshold && cpu_is_offline(i + 1)) {
 			dprintk("%s : bringing back cpu%d\n", THUNDERPLUG, i);
 			if (!((i + 1) > 7)) {
 				last_time[i + 1] = ktime_to_ms(ktime_get());
 				cpu_up(i + 1);
 			}
 		} else if (cpu_online(i) && avg_load[i] <
-				load_threshold && cpu_online(i + 1)) {
+				thunder_param.load_threshold && cpu_online(i + 1)) {
 			dprintk("%s : offlining cpu%d\n", THUNDERPLUG, i);
-			if (nr_cpu_online > min_core_online) {
+			if (nr_cpu_online >= thunder_param.min_core_online) {
+				/*
+				 * check if core touch boosted
+				 * before cpu_down
+				 */
+				time_now = ktime_to_us(ktime_get());
+				if (nr_cpu_online <=
+						thunder_param.cpus_boosted &&
+						(time_now -
+						thunder_param.last_input <
+						thunder_param.boost_lock_dur))
+					goto reschedule;
+
 				if (!(i + 1) == 0) {
 					now[i + 1] = ktime_to_ms(ktime_get());
 					if ((now[i + 1] - last_time[i + 1]) >
@@ -339,9 +420,129 @@ static void __cpuinit tplug_work_fn(struct work_struct *work)
 			}
 		}
 	}
+	reschedule:
 	queue_delayed_work_on(0, tplug_wq, &tplug_work,
-			msecs_to_jiffies(sampling_time));
+			msecs_to_jiffies(thunder_param.sampling_time));
 }
+
+static void __ref cpu_up_work(struct work_struct *work)
+{
+	int cpu;
+	unsigned int target = thunder_param.target_cpus;
+
+	for_each_cpu_not(cpu, cpu_online_mask) {
+		if (target <= num_online_cpus())
+			break;
+		if (cpu == 0)
+			continue;
+		cpu_up(cpu);
+	}
+}
+
+static void online_cpu(unsigned int target)
+{
+	unsigned int online_cpus = num_online_cpus();
+
+	/*
+	 * Do not online more CPUs if max_cpus_online reached
+	 * and cancel online task if target already achieved.
+	 */
+	if (target <= online_cpus ||
+			online_cpus >= thunder_param.max_core_online)
+		return;
+
+	thunder_param.target_cpus = target;
+	queue_work_on(0, tplug_wq, &thunder_param.up_work);
+}
+
+static void thunder_input_event(struct input_handle *handle, unsigned int type,
+				unsigned int code, int value)
+{
+	u64 time_now;
+
+	if (isSuspended)
+		return;
+	if (!thunder_param.tplug_hp_enabled)
+		return;
+
+	time_now = ktime_to_us(ktime_get());
+	thunder_param.last_input = time_now;
+	if (time_now - last_boost_time < MIN_INPUT_INTERVAL)
+		return;
+
+	if (num_online_cpus() >= thunder_param.cpus_boosted ||
+			thunder_param.cpus_boosted <=
+			thunder_param.min_core_online)
+		return;
+
+	online_cpu(thunder_param.cpus_boosted);
+	last_boost_time = ktime_to_us(ktime_get());
+}
+
+static int thunder_input_connect(struct input_handler *handler,
+				 struct input_dev *dev,
+				 const struct input_device_id *id)
+{
+	struct input_handle *handle;
+	int err;
+
+	handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
+	if (!handle)
+		return -ENOMEM;
+
+	handle->dev = dev;
+	handle->handler = handler;
+	handle->name = handler->name;
+
+	err = input_register_handle(handle);
+	if (err)
+		goto err_register;
+
+	err = input_open_device(handle);
+	if (err)
+		goto err_open;
+
+	return 0;
+err_open:
+	input_unregister_handle(handle);
+err_register:
+	kfree(handle);
+	return err;
+}
+
+static void thunder_input_disconnect(struct input_handle *handle)
+{
+	input_close_device(handle);
+	input_unregister_handle(handle);
+	kfree(handle);
+}
+
+static const struct input_device_id thunder_ids[] = {
+	{
+		.flags = INPUT_DEVICE_ID_MATCH_EVBIT |
+			 INPUT_DEVICE_ID_MATCH_ABSBIT,
+		.evbit = { BIT_MASK(EV_ABS) },
+		.absbit = { [BIT_WORD(ABS_MT_POSITION_X)] =
+			    BIT_MASK(ABS_MT_POSITION_X) |
+			    BIT_MASK(ABS_MT_POSITION_Y) },
+	}, /* multi-touch touchscreen */
+	{
+		.flags = INPUT_DEVICE_ID_MATCH_KEYBIT |
+			 INPUT_DEVICE_ID_MATCH_ABSBIT,
+		.keybit = { [BIT_WORD(BTN_TOUCH)] = BIT_MASK(BTN_TOUCH) },
+		.absbit = { [BIT_WORD(ABS_X)] =
+			    BIT_MASK(ABS_X) | BIT_MASK(ABS_Y) },
+	}, /* touchpad */
+	{ },
+};
+
+static struct input_handler thunder_input_handler = {
+	.event		= thunder_input_event,
+	.connect	= thunder_input_connect,
+	.disconnect	= thunder_input_disconnect,
+	.name		= THUNDERPLUG,
+	.id_table	= thunder_ids,
+};
 
 #ifdef CONFIG_STATE_NOTIFIER
 static void __ref thunderplug_suspend(void)
@@ -360,14 +561,14 @@ static void __ref thunderplug_resume(void)
 		isSuspended = false;
 		cpus_bring_online(1);
 		queue_delayed_work_on(0, tplug_wq, &tplug_work,
-				msecs_to_jiffies(sampling_time));
+				msecs_to_jiffies(thunder_param.sampling_time));
 	}
 }
 
 static int state_notifier_callback(struct notifier_block *this,
 				unsigned long event, void *data)
 {
-	if (!hotplug_suspend || !tplug_hp_enabled)
+	if (!thunder_param.hotplug_suspend || !thunder_param.tplug_hp_enabled)
 		return NOTIFY_OK;
 
 	switch (event) {
@@ -387,7 +588,7 @@ static int state_notifier_callback(struct notifier_block *this,
 static ssize_t thunderplug_hp_enabled_show(struct kobject *kobj,
 			struct kobj_attribute *attr, char *buf)
 {
-	return sprintf(buf, "%d", tplug_hp_enabled);
+	return sprintf(buf, "%d", thunder_param.tplug_hp_enabled);
 }
 
 static ssize_t __ref thunderplug_hp_enabled_store(struct kobject *kobj,
@@ -398,22 +599,23 @@ static ssize_t __ref thunderplug_hp_enabled_store(struct kobject *kobj,
 
 	sscanf(buf, "%d", &val);
 
-	last_val = tplug_hp_enabled;
+	last_val = thunder_param.tplug_hp_enabled;
 	switch(val) {
 		case 0:
 		case 1:
-			tplug_hp_enabled = val;
+			thunder_param.tplug_hp_enabled = val;
 			break;
 		default:
-			pr_info("%s : invalid choice\n", THUNDERPLUG);
+			pr_info("%s : invalid option set to hotplug enable\n",
+				THUNDERPLUG);
 			break;
 	}
 
-	if (tplug_hp_enabled == 1 && !last_val) {
+	if (thunder_param.tplug_hp_enabled == 1 && !last_val) {
 		thunderplug_start();
-	} else if (tplug_hp_enabled == 1 && last_val == 1) {
+	} else if (thunder_param.tplug_hp_enabled == 1 && last_val == 1) {
 		pr_info("%s : Already Working\n", THUNDERPLUG);
-	} else if (tplug_hp_enabled == 0 && last_val == 0) {
+	} else if (thunder_param.tplug_hp_enabled == 0 && last_val == 0) {
 		pr_info("%s : Already Offline\n", THUNDERPLUG);
 	} else {
 		thunderplug_stop();
@@ -463,6 +665,16 @@ static struct kobj_attribute thunderplug_load_attribute =
 		0666, thunderplug_load_show,
 		thunderplug_load_store);
 
+static struct kobj_attribute thunderplug_boost_lock_duration_attribute =
+	__ATTR(boost_lock_duration,
+		0666, thunderplug_boost_lock_duration_show,
+		thunderplug_boost_lock_duration_store);
+
+static struct kobj_attribute thunderplug_cpus_boosted_attribute =
+	__ATTR(cpus_boosted,
+		0666, thunderplug_cpus_boosted_show,
+		thunderplug_cpus_boosted_store);
+
 static struct attribute *thunderplug_attrs[] = {
 	&thunderplug_ver_attribute.attr,
 	&thunderplug_hotplug_suspend_attribute.attr,
@@ -471,6 +683,8 @@ static struct attribute *thunderplug_attrs[] = {
 	&thunderplug_sampling_attribute.attr,
 	&thunderplug_load_attribute.attr,
 	&thunderplug_hp_enabled_attribute.attr,
+	&thunderplug_boost_lock_duration_attribute.attr,
+	&thunderplug_cpus_boosted_attribute.attr,
 	NULL,
 };
 
@@ -491,6 +705,12 @@ static int __ref thunderplug_start(void)
 		ret = -ENOMEM;
 		goto err_out;
 	}
+	ret = input_register_handler(&thunder_input_handler);
+	if (ret) {
+		pr_err("%s: Failed to register input handler: %d\n",
+				THUNDERPLUG, ret);
+		return 0;
+	}
 
 #ifdef CONFIG_STATE_NOTIFIER
 	thunder_state_notif.notifier_call = state_notifier_callback;
@@ -502,6 +722,7 @@ static int __ref thunderplug_start(void)
 #endif
 
 	INIT_DELAYED_WORK(&tplug_work, tplug_work_fn);
+	INIT_WORK(&thunder_param.up_work, cpu_up_work);
 	for_each_possible_cpu(cpu) {
 		dl = &per_cpu(lock_info, cpu);
 		INIT_DELAYED_WORK(&dl->lock_rem, remove_down_lock);
@@ -531,7 +752,9 @@ static void thunderplug_stop(void)
 		dl = &per_cpu(lock_info, cpu);
 		cancel_delayed_work_sync(&dl->lock_rem);
 	}
+	input_unregister_handler(&thunder_input_handler);
 	flush_workqueue(tplug_wq);
+	cancel_work_sync(&thunder_param.up_work);
 	cancel_delayed_work_sync(&tplug_work);
 #ifdef CONFIG_STATE_NOTIFIER
 	state_unregister_client(&thunder_state_notif);
@@ -548,7 +771,7 @@ static void __init thunderplug_init(void)
 	int rc = sysfs_create_group(kernel_kobj, 
 			&thunderplug_attr_group);
 
-	if (tplug_hp_enabled)
+	if (thunder_param.tplug_hp_enabled)
 		thunderplug_start();
 
 	return 0;
@@ -556,7 +779,7 @@ static void __init thunderplug_init(void)
 
 static void __exit thunderplug_exit(void)
 {
-	if (tplug_hp_enabled)
+	if (thunder_param.tplug_hp_enabled)
 		thunderplug_stop();
 
 	sysfs_remove_group(kernel_kobj, &thunderplug_attr_group);
